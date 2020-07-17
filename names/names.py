e722b5d015be5cbd5b0f03234ffa375c3c15f1e5
lyndsiWilliams <kcatgirl@gmail.com>
2020-07-17 12:12:38 -0500

    Successfully implemented binary_search_tree to traverse through names

--- names/names.py
+++ names/names.py
@@ -1,4 +1,5 @@
 import time
+from binary_search_tree import BSTNode
 
 start_time = time.time()
 
@@ -12,11 +13,32 @@ f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
 
+# Create a new binary tree
+# The root of the tree is the first element of names_1
+new_tree = BSTNode(names_1[0])
+
+# Loop through names in names_1
+for name in names_1:
+    # Insert each name into the tree
+    new_tree.insert(name)
+
+# Loop through names in names_2
+for name in names_2:
+    # Check if the tree contains the name being checked
+    if new_tree.contains(name):
+        # If so, add it to the duplicates list
+        duplicates.append(name)
+
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+
+# --- STRETCH ---
+# duplicates.append(str(set(names_1).intersection(names_2)))
+
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
